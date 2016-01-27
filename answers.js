84d3cf67943cc3318864b2808bceda6b25e3c307
Phil Rajchgot <tophil@outlook.com>
2016-01-27 19:02:33 +0000

    Added add() - adds two numbers.

--- answers.js
+++ answers.js
@@ -32,3 +32,18 @@ console.log("chocolate -> " + charAt("chocolate", 3));
 console.log("starfish -> " + charAt("starfish", 6));
 console.log("empty string -> " + charAt("", 0));
 console.log();
+
+//takes two numbers and adds them together. 
+//When providing inputs other than numbers, unexpected results can occur, such as false + true giving 1.
+//If a string is provided, JS will convert the other input to a string and concatenate the two.
+function add (n1, n2) {
+    return n1 + n2;
+}
+
+console.log("Testing add()...");
+console.log("5 and 7 ... " + add(5,7));
+console.log("20 and 75,000,389 ... " + add(20, 75000389));
+console.log("0 and 0 ... " + add(0,0));
+console.log("false and true ... " + add(false, true));
+console.log("Watermelon and 9000... " + add("Watermelon", 9000));
+console.log();
