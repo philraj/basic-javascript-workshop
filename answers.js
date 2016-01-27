d38d5a6240285d3f90d58e022188b551bd7d8aa2
Phil Rajchgot <tophil@outlook.com>
2016-01-27 18:14:08 +0000

    Added lastChar() - outputs last character of a string. Also added some descriptive output text before each test.

--- answers.js
+++ answers.js
@@ -5,6 +5,17 @@ function firstChar (string) {
     return string[0];
 }
 
+console.log("Testing firstChar()...");
 console.log(firstChar("Cat"));
 console.log(firstChar('DecodeMTL'));
 console.log(firstChar(""));
+
+//takes a string and returns the last character of a string.
+function lastChar (string) {
+    return string[string.length - 1]
+}
+
+console.log("Testing lastChar()...");
+console.log(lastChar("JavaScript"));
+console.log(lastChar("explosion"));
+console.log(lastChar(""));
