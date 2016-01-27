8a267013fdc017d9f62e5c0a5013bae99398f9de
Phil Rajchgot <tophil@outlook.com>
2016-01-27 22:36:03 +0000

    Added quotes array

---
+++ fortune.js
@@ -0,0 +1,12 @@
+var quotes = [
+    "You will be very rich.",
+    "You will be very poor and miserable.",
+    "You will find your true love.",
+    "You will be alone forever.",
+    "You will travel the world.",
+    "You will remain stuck where you are.",
+    "You will do great things in life.",
+    "You will do nothing of significance.",
+    "This fortune teller will self-destruct in 5 seconds.",
+    "Try again later, I'm busy."
+]
