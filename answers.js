4dea3b0cf869247c07eba26ac314a0a51e5f6411
Phil Rajchgot <tophil@outlook.com>
2016-01-27 18:22:12 +0000

    Added charAt() - returns character at position provided in the string provided. Also made output text more descriptive.

--- answers.js
+++ answers.js
@@ -6,16 +6,29 @@ function firstChar (string) {
 }
 
 console.log("Testing firstChar()...");
-console.log(firstChar("Cat"));
-console.log(firstChar('DecodeMTL'));
-console.log(firstChar(""));
+console.log("Cat -> " + firstChar("Cat"));
+console.log("DecodeMTL -> " + firstChar('DecodeMTL'));
+console.log("empty string -> " + firstChar(""));
+console.log();
 
 //takes a string and returns the last character of a string.
 function lastChar (string) {
-    return string[string.length - 1]
+    return string[string.length - 1];
 }
 
 console.log("Testing lastChar()...");
-console.log(lastChar("JavaScript"));
-console.log(lastChar("explosion"));
-console.log(lastChar(""));
+console.log("JavaScript -> " + lastChar("JavaScript"));
+console.log("explosion -> " + lastChar("explosion"));
+console.log("empty string -> " + lastChar(""));
+console.log();
+
+//takes a string and a number, and returns the character at the position represented by the number. 
+function charAt (string, position) {
+    return string[position];
+}
+
+console.log("Testing charAt()...");
+console.log("chocolate -> " + charAt("chocolate", 3));
+console.log("starfish -> " + charAt("starfish", 6));
+console.log("empty string -> " + charAt("", 0));
+console.log();
