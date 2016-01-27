1923664ad18b179c67663e86584b4c5d325de466
Phil Rajchgot <tophil@outlook.com>
2016-01-27 18:09:22 +0000

    Added firstChar function (returns first character of a string)

--- answers.js
+++ answers.js
@@ -1,1 +1,10 @@
 // This is a one-line comment.
+
+//takes a string and returns the first character of the string.
+function firstChar (string) {
+    return string[0];
+}
+
+console.log(firstChar("Cat"));
+console.log(firstChar('DecodeMTL'));
+console.log(firstChar(""));
