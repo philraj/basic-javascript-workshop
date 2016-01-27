bdad6235cbab34d4a33b380c6a4d15589470781b
Phil Rajchgot <tophil@outlook.com>
2016-01-27 19:11:28 +0000

    Added multiply() - multiplies two numbers. Also reduced line widths for better readability.

--- answers.js
+++ answers.js
@@ -22,7 +22,8 @@ console.log("explosion -> " + lastChar("explosion"));
 console.log("empty string -> " + lastChar(""));
 console.log();
 
-//takes a string and a number, and returns the character at the position represented by the number. 
+//takes a string and a number, and returns the character at the position 
+//represented by the number. 
 function charAt (string, position) {
     return string[position];
 }
@@ -34,8 +35,9 @@ console.log("empty string -> " + charAt("", 0));
 console.log();
 
 //takes two numbers and adds them together. 
-//When providing inputs other than numbers, unexpected results can occur, such as false + true giving 1.
-//If a string is provided, JS will convert the other input to a string and concatenate the two.
+//When providing inputs other than numbers, unexpected results can occur, such as 
+//false + true giving 1. If a string is provided, JS will convert the other input
+//to a string and concatenate the two.
 function add (n1, n2) {
     return n1 + n2;
 }
@@ -47,3 +49,19 @@ console.log("0 and 0 ... " + add(0,0));
 console.log("false and true ... " + add(false, true));
 console.log("Watermelon and 9000... " + add("Watermelon", 9000));
 console.log();
+
+//takes two numbers and multiplies them together.
+//When passing something other than a number, the result is either NaN, or if 
+//one of the input can be converted easily into a number, the JS engine will do 
+//so and proceed as normal. 
+function multiply (n1, n2) {
+    return n1 * n2;
+}
+
+console.log("Testing multiply()...")
+console.log("3 and 9... " + multiply(3,9))
+console.log("50 and 50000 ... " + multiply(50,50000))
+console.log("House and Plant ... " + multiply("House", "Plant"))
+console.log("5 and '5'... " + multiply(5,'5'))
+console.log()
+
